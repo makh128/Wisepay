20907113e9ddaf089fe663c78f12f0a207c69555
Mahmoud Khaled <114943206+mody128@users.noreply.github.com>
2024-02-27 16:15:28 +0200

    update files

--- wisepay.js
+++ wisepay.js
@@ -84,6 +84,7 @@ for (let i = 0; i < accordion.length; i++) {
 
 const logPeople = [
   {
+    name: "Mahmoud",
     userName: "mahmoud.mk507@gmail.com",
     password: "mody",
   },
@@ -96,6 +97,8 @@ const logPeople = [
 function getInfo() {
   const signEmail = document.querySelector(".sign-email").value;
   const signPassword = document.querySelector(".sign-password").value;
+  let isEmailCorrect = false;
+  let isPasswordCorrect = false;
 
   for (let i = 0; i < logPeople.length; i++) {
     if (
@@ -105,9 +108,25 @@ function getInfo() {
       localStorage.setItem("isLoggedIn", true); // Store login status
       window.location.href = "index.html"; // Redirect to index.html
       return;
+    } else if (signEmail === logPeople[i].userName) {
+      isEmailCorrect = true;
+    } else if (signPassword === logPeople[i].password) {
+      isPasswordCorrect = true;
     }
   }
-  console.log("Invalid email or password");
+
+  // After checking all users, if email is incorrect
+  if (!isEmailCorrect) {
+    const feedback = document.querySelector(".invalid-feedback.email");
+    feedback.style.display = "block";
+  }
+
+  // After checking all users, if password is incorrect
+  if (!isPasswordCorrect) {
+    const feedback = document.querySelector(".invalid-feedback.password");
+    feedback.style.display = "block";
+  }
+
   localStorage.setItem("isLoggedIn", false); // Store login status
 }
 
@@ -115,18 +134,31 @@ document.addEventListener("DOMContentLoaded", function () {
   const isLoggedIn = localStorage.getItem("isLoggedIn");
   const sageDiv = document.querySelector(".sage");
   const sageInputs = document.querySelector(".sage-inputs");
-  const userName = document.querySelector(".welcome-name");
+  const welcomeName = document.querySelector(".welcome-name");
   const userWelcome = document.querySelector(".user-welcome");
   const userSigned = document.querySelector(".signed-user");
   const signBtn = document.querySelector(".sign-btn");
+  const userInfo = document.querySelector(".user-info");
+  const welName = document.querySelector(".wel-name");
+  const userNick = document.querySelector(".user-nick");
+  const userEmail = document.querySelector(".user-email");
 
   if (isLoggedIn) {
     signBtn.style.display = "none";
     sageDiv.classList.add("signed");
     userWelcome.classList.add("user-name");
-    userName.textContent = `Hello , ${logPeople[0].userName}`;
+    welcomeName.textContent = `Hello , ${logPeople[0].name}`;
     sageInputs.style.display = "none";
     userSigned.style.display = "block";
     userSigned.classList.remove("d-none");
+    userInfo.classList.remove("d-none");
+    welName.textContent = logPeople[0].name;
+    userNick.textContent = logPeople[0].name;
+    userEmail.textContent = logPeople[0].userName;
   }
 });
+
+function handelSignOut() {
+  localStorage.removeItem("isLoggedIn");
+  location.reload();
+}
